d3f2bc3c323a97d91889f482c777bea44f641ced
PingDocs2534 <docs4ping@gmail.com>
2021-01-17 22:21:26 +0700

    create

---
+++ SessionSample/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SessionSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using SessionSample.Extensions;
+using Microsoft.AspNetCore.Http;
+
+namespace SessionSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var list = new List<int>()
+            {
+                5,67,9,8
+            };
+            HttpContext.Session.Set<List<int>>("data", list);
+
+            var val = HttpContext.Session.Get<List<int>>("data");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
