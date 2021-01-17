9604b81117003f5f55e43cdb52753cc91cfd9be8
PingDocs2534 <docs4ping@gmail.com>
2021-01-17 13:07:59 +0700

    Created

---
+++ WebSiteCore3Samples/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebSiteCore3Samples.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
